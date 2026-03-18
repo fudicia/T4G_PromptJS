90c81e11e5f59309ed99c254b454a46ece5e2ca0
AbigailAwuni0 <abigailawuni0gmail.com>
2026-03-18 10:18:21 -0400

    adding email

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 let username = prompt("what is your name?");
 alert("Hello, " + username + "!");
+
 let age = prompt("What is your age?");
 alert("You are " + age + " years old.");
+
+let email = prompt("What is your email address?");
+alert("Your email address is " + email + ".");
+
+prompt(`${username} you are ${age} years old and we will keep in touch via your email: ${email}`)
 
